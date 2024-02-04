08982dcc4cb01cb0e390f785778f5bbc9675d903
kalifkhan <kalifkhan1998@gmail.com>
2024-02-04 23:43:33 +0530

    Updating

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,9 +1,29 @@
-import React from 'react'
+// Profile.js
+
+import React from 'react';
+import './Profile.css'; // Make sure to create this CSS file
 
 const Profile = () => {
   return (
-    <div>Profile</div>
-  )
-}
+    <div className="profile-container">
+      <div className="profile-image">
+        <img
+          src="https://placekitten.com/200/200" // Replace with the actual image URL
+          alt="Profile"
+        />
+      </div>
+      <div className="profile-details">
+        <h2>John Doe</h2>
+        <p className='p-Id'>ID: @john_doe</p>
+        <div className='profileData' >
+        <p className='followers'>Followers: 1000</p>
+        <p className='following'>Following: 500</p>
+        <p className='rating'>Rating: 8.5</p>
+        </div>
+        
+      </div>
+    </div>
+  );
+};
 
-export default Profile
+export default Profile;
