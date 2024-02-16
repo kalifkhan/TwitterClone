48183d9319d8fab35c182f9ea46c762e9a8e2c03
kalifkhan <kalifkhan1998@gmail.com>
2024-02-16 23:24:12 +0530

    Updating

--- src/components/Messages.js
+++ src/components/Messages.js
@@ -2,8 +2,11 @@ import React from 'react'
 
 const Messages = () => {
   return (
-    <div className='emptymsg'>Oops! Looks like our messaging pigeons are on a coffee break.
+    <div>
+      <div className='emptymsg'>Oops! Looks like our messaging pigeons are on a coffee break.
     They'll be back shortly with your messages.</div>
+    
+    </div>
     
   )
 }
