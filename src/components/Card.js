4fa7328dfa3a4c35c5867f8e477e2589749e8edf
kalifkhan <kalifkhan1998@gmail.com>
2024-03-16 20:02:53 +0530

    Updating

--- src/components/Card.js
+++ src/components/Card.js
@@ -2,22 +2,18 @@ import React, { useState, useEffect } from 'react';
 import './card.css';
 import { useNewTwitteContext } from './ContextList/TwitterContext';
 import { twitterIcon, messageIcon, retweetIcon, likesIcon , bookmark} from './leftPane/icons';
-
 export const Card = ({ title, content, id , islike}) => {
   const [like, setLike] = useState(false);
   const { updateHitLikes } = useNewTwitteContext();
   //const [bookMarkedList, setBookmarked] = useState([]);
   const { hitLikes, ListOfTwittes , state , dispatch } = useNewTwitteContext();
-
   const updatingLiked = (id) => {
     // dispatch( {type: "ADDLIKES" , payload: id , red: true , content: content})
     dispatch( { type: "ADDLIKESTOBOOKLIST" , id: id , islikevalue : false} )
   }
-
   const deletingLiked = (id) => {
     dispatch( { type: "ADDLIKESTOBOOKLIST" , id: id , islikevalue : false} )
   }
-
   const handleLikes = () => {
     setLike(!like);
     console.log("button liked")
@@ -36,7 +32,6 @@ export const Card = ({ title, content, id , islike}) => {
         <span className='svgicons'>
           <span>{messageIcon}</span>
           <span> {retweetIcon}</span>
-         
           <span className={like ? "likesicon" : ""} onClick={handleLikes}> {likesIcon}  </span>
           <span onClick={() => handleBookmarks(id)} > {bookmark}</span>
         </span>
