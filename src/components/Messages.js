9273e19a3606f642551d90b5c2b8a4d196e87348
kalifkhan <kalifkhan1998@gmail.com>
2024-03-20 20:56:25 +0530

    Updating

--- src/components/Messages.js
+++ src/components/Messages.js
@@ -6,7 +6,6 @@ const Messages = () => {
       <div className='emptymsg'>Oops! Looks like our messaging pigeons are on a coffee break.
     They'll be back shortly with your messages.</div>
     </div>
-    
   )
 }
 
